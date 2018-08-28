963d6d3fe142a448ce17323b875dbf51be67fd3f
wzl <wzlitcast@163.com>
2018-08-28 19:47:49 +0800

    1

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         System.out.println("1");
+        System.out.println("man1");
         System.out.println("2");
     }
 }
